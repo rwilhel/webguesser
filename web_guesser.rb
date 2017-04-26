4931854fbf82f156339800a466a1cb02008a256e
rwilhel <rwilhel@gmail.com>
2017-04-25 22:03:57 -0600

    I4: Extensions implementation - Colorize Output

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,13 +5,15 @@ require 'pry'
 NUMBER = rand(100)
 guess = nil
 message = "Guess a secret number between 0 and 100!"
+color = "teal"
 
 get '/' do
   if params["guess"] != nil
     guess = params["guess"].to_i
     message = check_guess(guess)
+    color = color_change(message)
   end
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  erb :index, :locals => {:number => NUMBER, :message => message, :color => color}
 end
 
 def check_guess(guess)
@@ -25,5 +27,16 @@ def check_guess(guess)
     "Too low!"
   else guess = NUMBER
     "You got it right!\n The SECRET number is #{NUMBER}."
+  end
+end
+
+def color_change(message)
+  case message
+  when "You got it right!\n The SECRET number is #{NUMBER}." then "green"
+  when "Too low!" then "pink"
+  when "Way too low!" then "red"
+  when "Too high!" then "pink"
+  when "Way too high!" then "red"
+  else "teal"
   end
 end
